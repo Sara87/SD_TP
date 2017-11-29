f94d0c7aa56f66e170546433ea6c6035ad7604c6
Sara Pereira <saralexx@gmail.com>
2017-11-29 20:03:34 +0000

    cenas

--- src/Servidor/OverBlind.java
+++ src/Servidor/OverBlind.java
@@ -7,8 +7,10 @@ import java.util.concurrent.locks.ReentrantLock;
 public class OverBlind {
     private Map<String,User> users;
     private Map<String, Heroe> heroes;
+    private Map<Integer, Game> games;
     private ReentrantLock userLock;
     private ReentrantLock heroeLock;
+    private ReentrantLock gameLock;
 
     public OverBlind(){}
 }
