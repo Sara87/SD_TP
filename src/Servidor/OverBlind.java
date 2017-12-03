773e1fc7f0348464bbdfcb85e12782d5ff4b70c2
adrianasg122 <adrianaguedes13@gmail.com>
2017-12-03 17:54:22 +0000

    login

--- src/Servidor/OverBlind.java
+++ src/Servidor/OverBlind.java
@@ -12,4 +12,42 @@ public class OverBlind {
     private ReentrantLock gameLock;
 
     public OverBlind(){}
+
+    /**
+     * Login do utilizador na plataforma.
+     *
+     * @param username Username do novo registo.
+     * @param password Password do novo registo.
+     */
+    public synchronized User login (String username, String password) throws UserInvalidException {
+        User u = new User();
+        try {
+            u = validaUtilizador(username, password);
+        } catch (Exception e) {
+            throw new UserInvalidException(e.getMessage());
+        }
+        return u;
+    }
+
+    /**
+     * Verifica se os dados do utilizador estão corretos
+     *
+     * @param username Username do utilizador
+     * @param password password do utilizador
+     */
+    public User validaUtilizador(String username, String password) throws UserInvalidException {
+        User u = null;
+        userLock.lock();
+        try {
+            for (String aux : users.keySet()) {
+                if ((aux.equals(username)) && users.get(aux).comparePass(password))
+                    u = this.users.get(aux);
+            }
+        }finally{
+            userLock.unlock();
+            if (u == null) throw new UserInvalidException("O utilizador não existe");
+        }
+        return u;
+    }
+
 }
